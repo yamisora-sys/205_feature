cf64d6d3bd5c7dec3cbe7bb4ec8964b719abb13a
dolamm <dolamm1509@gmail.com>
2022-10-07 10:57:03 +0700

    add filter file extension

--- src/components/AudioStorage.js
+++ src/components/AudioStorage.js
@@ -2,14 +2,20 @@ import { app} from './Firebase';
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const storage = getStorage(app);
+var acceptFile = ['mp4','mp3','acc','m4a']
 
 const uploadAudio = () => {
     const fileName = new Date();
     const audioRef = ref(storage, `audio/${fileName}`);
     const file = document.getElementById('audio').files[0];
-    uploadBytes(audioRef, file).then(() => {
-        alert("Audio uploaded successfully");
-    });
+    const fileExtension = file.name.split('.').pop();
+    if (acceptFile.includes(fileExtension)) {
+        uploadBytes(audioRef, file).then(() => {
+            alert("Audio uploaded successfully");
+        });
+    } else {
+        alert("Please upload a valid audio file");
+    }
 };
 
 export function AudioStorage(){
--- src/components/ImageStorage.js
+++ src/components/ImageStorage.js
@@ -3,26 +3,29 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const storage = getStorage(app);
 
+var acceptFile = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'tiff', 'psd', 'raw', 'heif', 'indd', 'svg', 'ai', 'eps', 'pdf', 'heic'];
+
 const uploadImage = () => {
     const fileName = new Date();
     const imgRef = ref(storage, `images/${fileName}`);
     const file = document.getElementById('image').files[0];
-    uploadBytes(imgRef, file).then(() => {
-        alert("Image uploaded successfully");
-    });
+    const fileExtension = file.name.split('.').pop();
+    if (acceptFile.includes(fileExtension)) {
+        uploadBytes(imgRef, file).then(() => {
+            alert("Image uploaded successfully");
+        });
+    } else {
+        alert("Please upload a valid image file");
+    }
 };
 
 export function ImageStorage(){
     return (
         <div>
-            <div>
+        <div>
             <input type="file" id="image" accept='image/*'/>
             <button onClick={uploadImage}>Upload Image</button>
         </div>
-        {/* <div>
-            <input type="file" id="audio"/>
-            <button onClick={uploadAudio}>Upload Audio</button>
-        </div> */}
         </div>
     )
 };
